0f7b80b99ec1bcedacef5bc335494e0ce62ffff0
Samuel barengo <samuk-09@hotmail.com>
2025-03-20 00:10:18 -0300

    animacao ao scroll refeita

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -4,29 +4,44 @@ export default class AnimaScroll {
         this.sections = document.querySelectorAll(sections)
         this.windowMetade = window.innerHeight * 0.6;
 
-        this.animaScroll = this.animaScroll.bind(this);
+        this.checkDistance = this.checkDistance.bind(this);
     }
-    // const sections = document.querySelectorAll('[data-scroll="scroll"]');
-
-    // const windowMetade = window.innerHeight * 0.6;
-    animaScroll() {
+    getDistance() { // Desestruturando uma node-List para array
+        this.distance = [...this.sections].map((section) => {
+            const offset = section.offsetTop;
+            return {
+                element: section,
+                offset: Math.floor(offset - this.windowMetade),
+            };
+        });
+    }
+    checkDistance() {
         this.sections.forEach((section) => {
             const sectionTop = section.getBoundingClientRect().top;
-            const isSectionVisible = (sectionTop - this.windowMetade < 0);
-
-            if (isSectionVisible)
-                section.classList.add('ativo');
-
-            else if (section.classList.contains('ativo')) {
-                section.classList.remove('ativo');
+            if (sectionTop < this.windowMetade) {
+                section.classList.add("ativo");
+            } else {
+                section.classList.remove("ativo");
             }
         });
     }
+
+    // checkDistance() {
+    //     this.distance.forEach(item => {
+    //         if (window.pageYOffset > item.offset) {
+    //             item.element.classList.add('ativo');
+    //         }
+    //         else if (item.element.classList.contains('ativo')) {
+    //             item.element.classList.remove('ativo');
+    //         }
+    //     });
+    // }
     init() {
-        this.animaScroll()
-        window.addEventListener('scroll', this.animaScroll);
+        if (this.sections.length) {
+            this.getDistance();
+            this.checkDistance();
+            window.addEventListener('scroll', this.checkDistance);
+        }
+        return this;
     }
 }
-// if(this.sections.length) {
-//     this.animaScroll();
-// }

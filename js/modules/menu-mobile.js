181180ab1f8d0135cf291eaeb4aecb12a934c857
Samuel barengo <samuk-09@hotmail.com>
2025-04-02 00:38:19 -0300

    fiz nada

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -6,7 +6,7 @@ export default class MenuMobile {
         this.activeClass = 'active';
 
         if (events === undefined ) this.events = ['touchstart', 'click'];
-        else this.events = events
+        else this.events = events;
 
         this.openMenu = this.openMenu.bind(this);
     }

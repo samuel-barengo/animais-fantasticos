85f2025811215b633e825a2096a5463808cb0f4c
Samuel barengo <samuk-09@hotmail.com>
2025-04-02 13:58:48 -0300

    acertado o caminho da Api

--- js/script.js
+++ js/script.js
@@ -37,5 +37,5 @@ menuMobile.init()
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('..//.//animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fecthBitcoin('https://blockchain.info/ticker');
